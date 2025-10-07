7b59c3b82c06b0fb38c7679af4c772b9c04d8222
Tyler Ptak <tylerptak@gmail.com>
2025-10-07 16:12:49 -0400

    Node forwarding Server

--- backend/src/signaling/server.js
+++ backend/src/signaling/server.js
@@ -1,11 +1,35 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
+const clients = new Set();
 
 wss.on('connection', function connection(ws) {
+  console.log('A new client connected!');
+  clients.add(ws);
+
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    try {
+      let message = JSON.parse(raw.toString())
+      console.log('received: %s', message);
+      // Send to gRPC server here
+      //grpcClient.sendMessage(message)
+      //broadcast the response to all other clients
+      ws.send(JSON.stringify({ type: 'echo', payload: message.payload }));
+    } catch (error) {
+      console.error('Error parsing message:', error);
+    }
   });
 
-  ws.send('something');
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clients.delete(ws);
+  })
+
+  ws.onerror = function () {
+    console.log('Some Error occurred')
+  }
+
+  // Send a message to the client
+
+  //ws.send('something');
 });
