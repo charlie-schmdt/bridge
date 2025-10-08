5c33e719168f0e374bcaad7dbfbaa7e35b05f730
mjsalasol <msalasol@purdue.edu>
2025-10-08 13:59:11 -0400

    Simple chat form on test room, icon change for inactive/active, window className tags

--- electron-src/src/renderer/RoomLayout.tsx
+++ electron-src/src/renderer/RoomLayout.tsx
@@ -8,14 +8,18 @@ import {useVideoFeedContext, VideoFeedContext} from "./contexts/VideoFeedContext
 import { VideoFeedType } from "./contexts/VideoFeedContext";
 import { useContext, useState } from "react";
 import { VideoFeedProvider } from "./providers/VideoFeedProvider";
+import Chat from "./components/Chat";
 
   
   interface RoomLayoutProps{}
 
 export default function RoomLayout({}: RoomLayoutProps){  
-
+  const [isChatOpen, setIsChatOpen ] = useState(false);
+  const openChat = () => setIsChatOpen(true);
+  const closeChat = () => setIsChatOpen(false);
 
   const navigate = useNavigate();
+  
   
   
 
@@ -38,14 +42,15 @@ export default function RoomLayout({}: RoomLayoutProps){
               </button>
           </header>
           <h1>Test Room</h1>
+          
           <VideoFeedProvider>
-            <div className="w-full max-w-3/4 h-full max-h-3/4">
-            
+            <div className="flex-1">
               <VideoFeed />
-            
-          </div>
-          <CallSettingsFooter />
+            </div>
+            <CallSettingsFooter onOpenChat={openChat} />
+            <Chat isOpen={isChatOpen} onClose={closeChat} />
           </VideoFeedProvider>
+
           
       </CardBody>
     </Card>
