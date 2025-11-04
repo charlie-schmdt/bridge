70aa10b75431e208795b1f9f8ed99af0536f149d
charlie-schmdt <cschmidt5207@gmail.com>
2025-11-04 09:04:48 -0500

    Refactor endpoints

--- electron-src/src/utils/endpoints.ts
+++ electron-src/src/utils/endpoints.ts
@@ -1,14 +1,15 @@
-const BACKEND = "http://localhost:8080"
+const BACKEND = "localhost:8080";
+const HTTP_BACKEND = "http://" + BACKEND;
 
-export const WebSocketURL = "ws://localhost:8080/ws"
+export const WebSocketURL = `ws://${BACKEND}/ws`;
 export const Endpoints = {
-  WORKSPACES_PUBLIC: BACKEND + "/api/workspaces/public",
-  USERS: BACKEND + "/api/users",
-  SETTINGS: BACKEND + "/api/settings",
-  AUTH_DELETE: BACKEND + "/api/auth/delete-account",
-  WORKSPACES: BACKEND + "/api/workspaces",
-  AUTH_OAUTH: BACKEND + "/api/auth/oauth",
-  AUTH_LOGIN: BACKEND + "/api/auth/login",
-  AUTH_REGISTER: BACKEND + "/api/auth/register",
-  AUTH_ACCOUNT: BACKEND + "/api/auth/account"
+  WORKSPACES_PUBLIC: HTTP_BACKEND + "/api/workspaces/public",
+  USERS: HTTP_BACKEND + "/api/users",
+  SETTINGS: HTTP_BACKEND + "/api/settings",
+  WORKSPACES: HTTP_BACKEND + "/api/workspaces",
+  AUTH_DELETE: HTTP_BACKEND + "/api/auth/delete-account",
+  AUTH_OAUTH: HTTP_BACKEND + "/api/auth/oauth",
+  AUTH_LOGIN: HTTP_BACKEND + "/api/auth/login",
+  AUTH_REGISTER: HTTP_BACKEND + "/api/auth/register",
+  AUTH_ACCOUNT: HTTP_BACKEND + "/api/auth/account"
 };
